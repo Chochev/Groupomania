21fbbdb71366184f3a9f0c57ddc4eaf9265f532f
Ivan Tchotchev <ivan_dirst@yahoo.com>
2022-01-11 10:04:32 +0100

    upload

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require("path");
+
+const app = express();
+const routesPosts = require('./routes/routesPosts');
+const routesUsers = require('./routes/routesUsers');
+const routesMod = require('./routes/routesMod');
+
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/posts', routesPosts);
+app.use('/api/auth', routesUsers);
+app.use('/api/moderation', routesMod);
+
+module.exports = app;
---
+++ backend/connectdb.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+console.log('connectdb');
+let connectdb = mysql.createConnection({ 
+    host: 'localhost', 
+    user: 'root', 
+    password: 'P@ssword85', 
+    database: 'grupomania', 
+});
+connectdb.connect(function(err) { 
+    if (err) throw err;
+    console.log('OK!')
+});
+
+module.exports = connectdb;
