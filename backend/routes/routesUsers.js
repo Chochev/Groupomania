329fa952cdaac678472c57b6ac2a884ad087ce73
Ivan Tchotchev <ivan_dirst@yahoo.com>
2022-01-16 11:01:19 +0100

    routes avatar

--- backend/routes/routesUsers.js
+++ backend/routes/routesUsers.js
@@ -1,19 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
-const userCtrl = require('../controllers/user');
+const userCtrl = require("../controllers/user");
 
-try{
-    router.post('/signup', userCtrl.signup);
-    router.post('/login', userCtrl.login);
-    router.get('/', auth, userCtrl.seeMyProfile);
-    router.delete('/', auth, userCtrl.deleteUser);
-    router.put('/', auth, userCtrl.updateUser);
-}catch (error){
-    console.log(error);
+try {
+  router.post("/signup", userCtrl.signup);
+  router.post("/login", userCtrl.login);
+  router.get("/", auth, userCtrl.seeMyProfile);
+  router.delete("/", auth, userCtrl.deleteUser);
+  router.put("/", auth, userCtrl.updateUser);
+  router.post("/avatar", auth, multer.single("avatar"), userCtrl.setAvatar);
+} catch (error) {
+  console.log(error);
 }
 
 module.exports = router;
