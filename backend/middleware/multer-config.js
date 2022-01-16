f938569133ea34749bc2a7431400e8b08836e4aa
Ivan Tchotchev <ivan_dirst@yahoo.com>
2022-01-16 11:03:48 +0100

    config changes

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,22 +1,22 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 
 const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png'
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
 };
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, 'images');
+    callback(null, "./images");
   },
   filename: (req, file, callback) => {
     const extPath = path.extname(`/images/${file.originalname}`);
-    const name = file.originalname.split(' ').join('_').split(extPath).join('');
+    const name = file.originalname.split(" ").join("_").split(extPath).join("");
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + '_' + Date.now() + '.' + extension);
-  }
+    callback(null, name + "_" + Date.now() + "." + extension);
+  },
 });
 
-module.exports = multer({storage: storage}).single('image');
+module.exports = multer({ storage: storage });
