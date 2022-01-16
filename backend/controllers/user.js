481c4c05b6d9e62a786554db439f262dd4681ae1
Ivan Tchotchev <ivan_dirst@yahoo.com>
2022-01-16 11:03:01 +0100

    avatar logic

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,88 +1,96 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const connectdb = require('../connectdb.js');
-const mysql = require('mysql2');
-const UserManager = require ('../managers/UserManager.js')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const connectdb = require("../connectdb.js");
+const mysql = require("mysql2");
+const UserManager = require("../managers/UserManager.js");
 
 let userManager = new UserManager();
 
-
 exports.signup = (req, res, next) => {
-    let email = req.body.email;
-	let password = req.body.password;
-	let firstName = req.body.firstName;
-    let lastName = req.body.lastName;
-    bcrypt.hash(password, 10)
-        .then (hash => {
-            let sqlInserts = [lastName, firstName, email, hash];
-            userManager.signup(sqlInserts)
-                .then((response) =>{
-                    res.status(201).json(JSON.stringify(response))
-                })
-                .catch((error) =>{
-                    console.error(error);
-                    res.status(400).json({error})
-                })
+  let email = req.body.email;
+  let password = req.body.password;
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => {
+      let sqlInserts = [lastName, firstName, email, hash];
+      userManager
+        .signup(sqlInserts)
+        .then((response) => {
+          res.status(201).json(JSON.stringify(response));
         })
-        .catch(error => res.status(500).json(error)) 
+        .catch((error) => {
+          console.error(error);
+          res.status(400).json({ error });
+        });
+    })
+    .catch((error) => res.status(500).json(error));
+};
+//avatar image
+exports.setAvatar = (req, res, next) => {
+  console.log(req);
+  // TODO: handle db update... (req.file.filename) `${req.file/destination}/${req.file.filename}`
 };
 
-
 exports.login = (req, res, next) => {
-    let email = req.body.email;
-    let password = req.body.password;
-    let sqlInserts = [email];
-    userManager.login(sqlInserts, password)
-        .then((response) =>{
-            res.status(200).json(JSON.stringify(response))
-        })
-        .catch((error) =>{
-            res.status(400).json(error)
-        })
-}
+  let email = req.body.email;
+  let password = req.body.password;
+  let sqlInserts = [email];
+  userManager
+    .login(sqlInserts, password)
+    .then((response) => {
+      res.status(200).json(JSON.stringify(response));
+    })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
+};
 exports.seeMyProfile = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    const userId = decodedToken.userId;
-    let sqlInserts = [userId];
-    userManager.seeMyProfile(sqlInserts)
-        .then((response) =>{
-            res.status(200).json(JSON.stringify(response))
-        })
-        .catch((error) =>{
-            console.log(error);
-            res.status(400).json(error)
-        })
-}   
+  const token = req.headers.authorization.split(" ")[1];
+  const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+  const userId = decodedToken.userId;
+  let sqlInserts = [userId];
+  userManager
+    .seeMyProfile(sqlInserts)
+    .then((response) => {
+      res.status(200).json(JSON.stringify(response));
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).json(error);
+    });
+};
 exports.updateUser = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    const userId = decodedToken.userId;
-    let firstName = req.body.firstName;
-    let lastName = req.body.lastName;
-    let email = req.body.email;
-    let sqlInserts = [firstName, lastName, email, userId];
-    userManager.updateUser(sqlInserts)
-        .then((response) =>{
-            res.status(200).json(JSON.stringify(response))
-        })
-        .catch((error) =>{
-            res.status(400).json(error)
-        })
-}
- 
+  const token = req.headers.authorization.split(" ")[1];
+  const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+  const userId = decodedToken.userId;
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+  let sqlInserts = [firstName, lastName, email, userId];
+  userManager
+    .updateUser(sqlInserts)
+    .then((response) => {
+      res.status(200).json(JSON.stringify(response));
+    })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
+};
+
 exports.deleteUser = (req, res, next) => {
-    //let userId = req.params.id;
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    const userId = decodedToken.userId;
-    let sqlInserts = [userId];
-    userManager.deleteUser(sqlInserts)
-        .then((response) =>{
-            res.status(200).json(JSON.stringify(response))
-        })
-        .catch((error) =>{
-            console.log(error);
-            res.status(400).json(error)
-        })
-} 
+  const token = req.headers.authorization.split(" ")[1];
+  const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+  const userId = decodedToken.userId;
+  let sqlInserts = [userId];
+  userManager
+    .deleteUser(sqlInserts)
+    .then((response) => {
+      res.status(200).json(JSON.stringify(response));
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).json(error);
+    });
+};
