eaeadf5d2f32339acdbce953cd97d6d902cb3668
Ivan Tchotchev <ivan_dirst@yahoo.com>
2022-01-18 09:14:24 +0100

    add path to app

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
     next();
 });
 
-//app.use("/images",express.static(path.join(__dirname, "images")));
+app.use("/images",express.static(path.join(__dirname, "images")));
 app.use('/api/posts', routesPosts);
 app.use('/api/auth', routesUsers);
 app.use('/api/moderation', routesMod);
